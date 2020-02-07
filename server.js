e5b6689f9ad6b36e6ae0088c631120e8757f8acf
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2020-02-07 10:53:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/beaverworxModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/beaverworxdb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/beaverworxRoutes'); //importing route
+routes(app); //register the route
+
 
 app.listen(port);
+
 
 console.log('Beaverworx API server started on: ' + port);
