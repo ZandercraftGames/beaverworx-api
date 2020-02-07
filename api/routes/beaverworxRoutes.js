f7fe090fc615530ac1ebfdc3d137ed749fad50eb
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2020-02-07 10:55:08 -0500

    Fix variable

--- api/routes/beaverworxRoutes.js
+++ api/routes/beaverworxRoutes.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(app) {
-  var todoList = require('../controllers/beaverworxController');
+  var beaverworx = require('../controllers/beaverworxController');
 
   // todoList Routes
   app.route('/events')
